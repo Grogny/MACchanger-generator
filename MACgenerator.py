de4e819ec5a60423c1eab572268bf96d799edc74
Grogny <87657107+Grogny@users.noreply.github.com>
2023-04-30 17:45:24 +0200

    Create MACgenerator.py

---
+++ MACgenerator.py
@@ -0,0 +1,12 @@
+from randmac import RandMac
+
+while True:
+    nb = input("[+] How many address? (max : 9) : ")
+    if nb.isnumeric() == False:
+        print("Please enter a number")
+    elif int(nb) > 9:
+        print("Max : 9, please enter a valid ammount of address")
+    else:
+        for i in range(0, int(nb)):
+            print(f"[{i+1}]", RandMac("00:00:00:00:00:00"))
+        break
