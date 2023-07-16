94b8fa48929c3fea015adf4a5ca5efb9deb2ff0c
Grogny <87657107+Grogny@users.noreply.github.com>
2023-07-16 21:01:10 +0200

    Add files via upload

--- MACaddress.py
+++ MACaddress.py
@@ -1,32 +1,41 @@
+from colorama import Fore
 import subprocess
+import colorama
 import optparse
 
-print("    __  ___  ___     ______          __")
-print("   /  |/  / /   |   / ____/  _____  / /_   ____ _   ____    ____ _  ___    _____")
-print("  / /|_/ / / /| |  / /      / ___/ / __ \ / __ `/  / __ \  / __ `/ / _ \  / ___/")
-print(" / /  / / / ___ | / /___   / /__  / / / // /_/ /  / / / / / /_/ / /  __/ / /")
-print("/_/  /_/ /_/  |_| \____/   \___/ /_/ /_/ \__,_/  /_/ /_/  \__, /  \___/ /_/")
-print("                                                         /____/")
-
-def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface can be ether0 or wlan0")
-    parser.add_option("-m", "--macaddress", dest="mac_address", help="Your new MAC address")
-    (options, arguments) = parser.parse_args()
-
-    if not options.interface:
-        parser.error("[!] Next time, specify your interface, --help for more information")
-    elif not options.mac_address:
-        parser.error("[!] Next time, specify your MAC address, --help for more information")
-    return options
 
 def change_mac(interface, mac_address):
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", mac_address])
     subprocess.call(["ifconfig", interface, "up"])
     subprocess.call(["ifconfig", interface])
+    print(f"{Fore.LIGHTGREEN_EX}[+] {Fore.WHITE}Your new {Fore.RED}{interface} {Fore.WHITE}is now {Fore.RED}{mac_address} {Fore.WHITE}!")
 
-    print(f"[+] Your new {interface} is now {mac_address} !")
+    ifconfig = subprocess.check_output(["ifconfig", interface])
+    print(f"\n {Fore.WHITE}{ifconfig}")
 
-options = get_arguments()
-change_mac(options.interface, options.mac_address)
+def main():
+    colorama.init()
+    print(f"""
+{Fore.BLUE}    __  ___  ___     ______  {Fore.YELLOW}        __
+{Fore.BLUE}   /  |/  / /   |   / ____/  {Fore.YELLOW}_____  / /_   ____ _   ____    ____ _  ___    _____{Fore.WHITE}
+{Fore.BLUE}  / /|_/ / / /| |  / /      {Fore.YELLOW}/ ___/ / __ \ / __ `/  / __ \  / __ `/ / _ \  / ___/{Fore.WHITE}
+{Fore.BLUE} / /  / / / ___ | / /___   {Fore.YELLOW}/ /__  / / / // /_/ /  / / / / / /_/ / /  __/ / /{Fore.WHITE}
+{Fore.BLUE}/_/  /_/ /_/  |_| \____/   {Fore.YELLOW}\___/ /_/ /_/ \__,_/  /_/ /_/  \__, /  \___/ /_/{Fore.WHITE}
+{Fore.BLUE}                                                         {Fore.YELLOW}/____/{Fore.WHITE}
+""")
+    
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface can be ether0 or wlan0")
+    parser.add_option("-m", "--macaddress", dest="mac_address", help="Your new MAC address")
+    (options, arguments) = parser.parse_args() 
+
+    if not options.interface:
+        parser.error(f"{Fore.LIGHTGREEN_EX}[!] {Fore.WHITE}Next time, specify your {Fore.RED}interface{Fore.WHITE}, --help for more information")
+    elif not options.mac_address:
+        parser.error(f"{Fore.LIGHTGREEN_EX}[!] {Fore.WHITE}Next time, specify your {Fore.RED}MAC address{Fore.WHITE}, --help for more information")
+    else:
+        change_mac(options.interface, options.mac_address)
+
+if __name__ == "__main__":
+    main()
